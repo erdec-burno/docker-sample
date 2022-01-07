2e8e5cfce845aa9e917d6c36ba811c63d1c614d3
erdec <erdec@mail.ru>
2022-01-07 23:42:01 +0200

    add composer

--- www/public/index.php
+++ www/public/index.php
@@ -7,6 +7,6 @@ use Monolog\Handler\StreamHandler;
 
 $log = new Logger('message');
 $log->pushHandler(new StreamHandler(__DIR__.'/../logs.log'));
-$log->info('test message 3');
+$log->info('test message final! 2 :)');
 //echo config('DB_NAME');
 //echo phpinfo();
