d355773ded431baa74e1f4d681d41e2ba47f9627
erdec <erdec@mail.ru>
2022-01-10 23:01:54 +0200

    for test

--- www/public/index.php
+++ www/public/index.php
@@ -10,6 +10,4 @@ $log = new Logger('message');
 $log->pushHandler(new StreamHandler('/app/logs/logs.log'));
 $log->info('test message final! 2 :)');
 echo "data from DB: " .config('DB_NAME');
-$cities = Capsule::table('cities')->limit(3)->get();
-echo "<pre>" . $cities . "</pre>";
-//echo phpinfo();
+echo phpinfo();
